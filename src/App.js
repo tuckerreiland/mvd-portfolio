344e0e3d2dc5882296c5c3c4d413006ddc2ce629
tuckerreiland <treiland@uw.edu>
2023-01-11 12:19:38 -0800

    fixed

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl text-red-500">
+      <h1>
+		Hellow World
+	  </h1>
+    </div>
+  );
+}
+
+export default App;
